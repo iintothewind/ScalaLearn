2c9e5112152d8aadf1c439574a472caff8adc0f2
ivar <ivar.chen@gmail.com>
2025-01-06 22:18:43 -0800

    added StateMachine

--- src/main/scala/sm/StateMachine.scala
+++ src/main/scala/sm/StateMachine.scala
@@ -1,9 +1,40 @@
 package sm
 
+case class StateMachine[S, C, R](s: Class[S], c: Class[C], r: Class[R], transitions: Set[Transition[S, C, R]] = Set.empty[Transition[S, C, R]]) {
 
-sealed case class StateMachine[S, C, R] private(s: Class[S], c: Class[C], r: Class[R], transitions: List[Transition[S, C, R]]) {
+  def withTransition(from: S, to: S, predicate: C => Boolean, converter: C => R): StateMachine[S, C, R] = {
+    if (transitions.exists(t => t.fromState == from && t.toState == to)) {
+      throw new IllegalStateException(s"transition from: $from to: $to already exists")
+    } else {
+      val lst = transitions.incl(Transition(from, to, predicate, converter))
+      copy(transitions = lst)
+    }
+  }
 
-  def typeOf(s: Class[S], c: Class[C], r: Class[R]): StateMachine[S, C, R] = StateMachine(s, c, r, List.empty)
+  def withTransition(transition: Transition[S, C, R]): StateMachine[S, C, R] = Option(transition) match
+    case Some(t) => withTransition(t.fromState, t.toState, t.predicate, t.converter)
+    case _ => this
 
+  def withTransition(from: S, to: S, converter: C => R): StateMachine[S, C, R] = withTransition(from, to, _ => true, converter)
+
+  def withTransitions(trans: List[Transition[S, C, R]]): StateMachine[S, C, R] = Option(trans) match
+    case Some(lst) => if (lst.toSet.intersect(transitions).nonEmpty) {
+      throw new IllegalStateException(s"input list contains existing transitions")
+    } else {
+      val list = transitions.concat(lst)
+      copy(transitions = list)
+    }
+    case _ => this
+
+  def withTransitions(fromStates: List[S], to: S, predicate: C => Boolean, converter: C => R): StateMachine[S, C, R] = Option(fromStates) match
+    case Some(lst) => withTransitions(lst.map(from => Transition(from, to, predicate, converter)))
+    case _ => this
+
+  def withTransitions(from: S, toStates: List[S], predicate: C => Boolean, converter: C => R): StateMachine[S, C, R] = Option(toStates) match
+    case Some(lst) => withTransitions(lst.map(to => Transition(from, to, predicate, converter)))
+    case _ => this
+
+  def mkTransition(fromState: S, toState: S, context: C): R = transitions.find(t => t.fromState == fromState && t.toState == toState).map(t => t.execute(context)).getOrElse(null.asInstanceOf[R])
 
 }
+
--- src/test/scala/sm/TransitionTest.scala
+++ src/test/scala/sm/TransitionTest.scala
@@ -5,16 +5,30 @@ import org.scalatest.funsuite.AnyFunSuite
 
 import java.lang
 
-case class Order(id: Int, name: String, status: String)
+case class Order(id: Int, status: String)
 
 class TransitionTest extends AnyFunSuite, LazyLogging {
 
   test("init01") {
     val t = Transition("a", "b", (o: Order) => o.status == "a", (o: Order) => Some(o.copy(status = "b")))
-    val o = Order(1, "00a1", "a")
+    val o = Order(1, "a")
     assert(t.check(o))
     println(t.execute(o))
     assert(t.execute(o).map(o => o.status).orNull() == "b")
+  }
+
+  test("initSm01") {
+    val t: Transition[String, Order, Order] = Transition("a", "b", (o: Order) => o.status == "a", (o: Order) => o.copy(status = "b"))
+    val o = Order(1, "a")
+    val sm = StateMachine(s = classOf[String], c = classOf[Order], r = classOf[Order])
+      .withTransition(t)
+      .withTransition("a", "c", o => o.status == "a", o => o.copy(status = "c"))
+
+    val r1 = sm.mkTransition("a", "b", o)
+    assertResult(r1.status)("b")
+
+    val r2 = sm.mkTransition("a", "c", o)
+    assertResult(r2.status)("c")
   }
 
 }
